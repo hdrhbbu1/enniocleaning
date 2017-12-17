084cc6256292e17b852089439018e6d099eead7c
Ben Read <ben.read@indigotree.co.uk>
2017-12-17 21:34:10 +0000

    logo, font and styled-components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,70 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from "styled-components"
+import '../global-styles.js'
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: SakerSansBold, sans-serif;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+const NavFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 15px;
+  color: black;
+  transition: all 0.25s ease-in-out;
+  font-family: 'SakerSansBold', sans-serif;
+  font-size: 22px;
+
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: gray;
+  }
+	&.active {
+		background-color: #daaf09;
+	}
+
+  &:last-child {
+    background-color: #BF0B12;
+    color: black;
+
+    &:hover,
+    &:active,
+    &:focus {
+      background-color: #ad1b21;
+    }
+  }
+`;
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div>
-  			<Link activeClassName="active" to={`/`}>
+      <Nav>
+  			<StyledLink activeClassName="active" to={`/`}>
   				Home
-  			</Link>
-  			<Link activeClassName="active" to={`/about/`}>
+  			</StyledLink>
+  			<StyledLink activeClassName="active" to={`/about/`}>
   				About
-  			</Link>
-  			<Link activeClassName="active" to={`/services/`}>
+  			</StyledLink>
+  			<StyledLink activeClassName="active" to={`/services/`}>
   				Services
-  			</Link>
-  			<Link activeClassName="active" to={`/contact/`}>
+  			</StyledLink>
+  			<StyledLink activeClassName="active" to={`/contact/`}>
   				Contact
-  			</Link>
-  		</div>
+  			</StyledLink>
+  		</Nav>
     )
   }
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,9 +2,59 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import styled from "styled-components"
+import '../global-styles.js'
 
 import Navbar from '../components/Navbar'
 import logo from './ennioslogo.svg'
+
+const Wrapper = styled.section`
+	background: black;
+  font-family: sans-serif;
+`;
+
+const Container = styled.section`
+  padding: 2em;
+  max-width: 1200px;
+  margin: 0 auto;
+  color: white;
+`;
+const Main = styled.main`
+  @media (min-width: 768px) {
+    flex: 2;
+  }
+`;
+const Aside = styled.aside`
+  @media (min-width: 768px) {
+    flex: 1;
+  }
+`;
+const Header = styled.header`
+	background-color: rgba(255,255,255,0.2);
+`;
+const HeaderContainer = Container.extend`
+	padding: 0.2em;
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	padding: 1em;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Footer = Container.extend`
+
+	@media (min-width: 768px) {
+		display: flex;
+
+		> * {
+			flex: 1;
+			margin: 15px;
+		}
+	}
+`;
 
 class Template extends React.Component {
   render() {
